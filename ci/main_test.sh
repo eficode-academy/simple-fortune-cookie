2acda8e8d0b60d95f715d48f293a1a9d4a15b0c9
Mlinnelyst <mlinnelyst11@gmail.com>
2021-08-19 12:47:39 +0200

    a

--- ci/main_test.sh
+++ ci/main_test.sh
@@ -1,15 +1,3 @@
-set -eux
-
-declare -r HOST="localhost:8080"
-
-wait-for-url() {
-    echo "Testing $1"
-    timeout -s TERM 45 bash -c \
-    'while [[ "$(curl -s -o /dev/null -L -w ''%{http_code}'' ${0})" != "200" ]];\
-    do echo "Waiting for ${0}" && sleep 2;\
-    done' ${1}
-    echo "OK!"
-    curl -I $1
-}
-
-cd configuration && docker-compose up & cd .. && wait-for-url http://${HOST} && sleep 5 && go test ./frontend/main_test.go
+cd ./frontend
+go test
+cd ..
