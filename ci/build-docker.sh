37ec5311268d2545339cf98a96d4409a1062a51a
Ubuntu <ubuntu@devopsacademy-instance49.c.praqma-training.internal>
2021-08-19 12:13:31 +0000

    Adds pipeline 25. try

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::4}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-docker build -t "${DockerRepo}simple-fortune-cookie-app:latest" -t "${DockerRepo}simple-fortune-cookie-app:1.0-$Tag" app/
+docker build -t "${DockerRepo}simple-fortune-cookie-backend:latest" -t "${DockerRepo}simple-fortune-cookie-backend:1.0-$Tag" backend/
+docker build -t "${DockerRepo}simple-fortune-cookie-frontend:latest" -t "${DockerRepo}simple-fortune-cookie-frontend:1.0-$Tag" frontend/
--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "$docker_password" | docker login --username "$docker_username" --password-stdin
-docker push "$docker_username/simple-fortune-cookie-app:1.0-${GIT_COMMIT::4}" 
-docker push "$docker_username/simple-fortune-cookie-app:latest" &
+docker push "$docker_username/simple-fortune-cookie-backend:1.0-${GIT_COMMIT::4}" 
+docker push "$docker_username/simple-fortune-cookie-backend:latest"
+docker push "$docker_username/simple-fortune-cookie-frontend:1.0-${GIT_COMMIT::4}" 
+docker push "$docker_username/simple-fortune-cookie-frontend:latest" &
 wait
