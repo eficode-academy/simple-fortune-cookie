374f52e70d88730fed06b2eb1bbf62d89480190d
Mlinnelyst <mlinnelyst11@gmail.com>
2021-08-19 12:24:57 +0200

    .

--- ci/build-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::4}"
-[[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-
-
-#docker build -t simplefortunecookielmao/sfc ./frontend
--- ci/push-docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "$docker_password" | docker login --username "$docker_username" --password-stdin
-#docker push simplefortunecookielmao/sfc:tagname
-wait
