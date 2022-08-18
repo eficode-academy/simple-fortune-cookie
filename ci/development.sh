8499f0e030b037addf9a943dfebffd1ffeb9d0e4
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2022-08-18 19:32:55 +0200

    Added detached mode to all docker images (#21)
    * Gave permissions for github runner to run script
    
    * Added detached mode to all docker images

--- ci/development.sh
+++ ci/development.sh
@@ -1,2 +1,2 @@
-docker run ghcr.io/stefanowich33/simple-fortune-cookie/cookie-backend:trunk --pull=always
-docker run ghcr.io/stefanowich33/simple-fortune-cookie/cookie-frontend:trunk --pull=always
+docker run -d ghcr.io/stefanowich33/simple-fortune-cookie/cookie-backend:trunk --pull=always
+docker run -d ghcr.io/stefanowich33/simple-fortune-cookie/cookie-frontend:trunk --pull=always
--- ci/production.sh
+++ ci/production.sh
@@ -1,2 +1,2 @@
-docker run ghcr.io/stefanowich33/simple-fortune-cookie/cookie-backend:$0
-docker run ghcr.io/stefanowich33/simple-fortune-cookie/cookie-frontend:$0 --env BACKEND_DNS=backend
+docker run -d ghcr.io/stefanowich33/simple-fortune-cookie/cookie-backend:$0
+docker run -d ghcr.io/stefanowich33/simple-fortune-cookie/cookie-frontend:$0 --env BACKEND_DNS=backend
