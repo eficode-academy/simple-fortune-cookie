a0e3a406ad00fe966009825a03e45846b80976b6
Fuad <fujam20@student.sdu>
2022-08-18 12:29:56 +0000

    test

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-docker build -t "${DockerRepo}simple-fortune-cookie:latest" -t "${DockerRepo}simple-fortune-cookie:1.0-$Tag" app/
+docker build -t "${DockerRepo}simple-fortune-cookie-backend:latest" -t "${DockerRepo}simple-fortune-cookie:1.0-$Tag" backend/
+docker build -t "${DockerRepo}simple-fortune-cookie-frontend:latest" -t "${DockerRepo}simple-fortune-cookie:1.0-$Tag" frontend/
+
--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "$docker_password" | docker login --username "$docker_username" --password-stdin
-docker push "$docker_username/simple-fortune-cookie:1.0-${GIT_COMMIT::8}" 
-docker push "$docker_username/simple-fortune-cookie:latest" &
+docker push "$docker_username/simple-fortune-cookie-backend:1.0-${GIT_COMMIT::8}" 
+docker push "$docker_username/simple-fortune-cookie-frontend:latest" &
 wait
