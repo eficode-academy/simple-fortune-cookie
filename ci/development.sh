a94044d5d4c92fa6c3efd02349de405ee91b96f0
nidam16 <nidam16@student.sdu.dk>
2022-08-18 19:46:56 +0200

    Added names to docker deploys

--- ci/development.sh
+++ ci/development.sh
@@ -1,2 +1,3 @@
-docker run -d ghcr.io/stefanowich33/simple-fortune-cookie/cookie-backend:trunk --pull=always
-docker run -d ghcr.io/stefanowich33/simple-fortune-cookie/cookie-frontend:trunk --pull=always
+docker 
+docker run -d --name development-backend ghcr.io/stefanowich33/simple-fortune-cookie/cookie-backend:trunk --pull=always
+docker run -d --name development-frontend ghcr.io/stefanowich33/simple-fortune-cookie/cookie-frontend:trunk --pull=always
--- ci/staging.sh
+++ ci/staging.sh
@@ -1,2 +1,2 @@
-docker run -d ghcr.io/stefanowich33/simple-fortune-cookie/cookie-backend:$0
-docker run -d ghcr.io/stefanowich33/simple-fortune-cookie/cookie-frontend:$0 --env BACKEND_DNS=backend
+docker run -d --name staging-backend ghcr.io/stefanowich33/simple-fortune-cookie/cookie-backend:$0
+docker run -d --name staging-frontend ghcr.io/stefanowich33/simple-fortune-cookie/cookie-frontend:$0 --env BACKEND_DNS=backend
