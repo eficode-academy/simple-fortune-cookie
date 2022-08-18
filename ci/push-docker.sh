99a9b75fbc66ceb2e978c9d522d62de03709aa1d
Fuad <fujam20@student.sdu>
2022-08-18 12:37:30 +0000

    test

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "$docker_password" | docker login --username "$docker_username" --password-stdin
 docker push "$docker_username/simple-fortune-cookie-backend:1.0-${GIT_COMMIT::8}" 
+docker push "$docker_username/simple-fortune-cookie-backend:latest"
+docker push "$docker_username/simple-fortune-cookie-frontend:1.0-${GIT_COMMIT::8}" 
 docker push "$docker_username/simple-fortune-cookie-frontend:latest" &
+
 wait
