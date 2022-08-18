6a39819fcd02b07950a6bc419d5b3cfb25c2b16b
here <hendrik.reiter@ppi.de>
2022-08-18 11:31:59 +0200

    try docker fix

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-docker build -t "${DockerRepo}fortune-cookie-frontend:latest" -t "${DockerRepo}fortune-cookie-frontend:1.0-$Tag" -f ./frontend/Dockerfile
-docker build -t "${DockerRepo}fortune-cookie-backend:latest" -t "${DockerRepo}fortune-cookie-backend:1.0-$Tag" -f ./backend/Dockerfile
+docker build -t "${DockerRepo}fortune-cookie-frontend:latest" -t "${DockerRepo}fortune-cookie-frontend:1.0-$Tag" ./frontend/
+docker build -t "${DockerRepo}fortune-cookie-backend:latest" -t "${DockerRepo}fortune-cookie-backend:1.0-$Tag" ./backend/
