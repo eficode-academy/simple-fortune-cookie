b1763afe5124b631b711639a49a9d060bdb314f2
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2022-08-18 19:08:59 +0200

    Added Continous Delivery (#19)
    * Update test.yaml
    
    * Update test.yaml
    
    * Extended deployment script with
    
    * Changed if in deployment.yaml
    
    Co-authored-by: Frederik Hounsvad <36563020+Hounsvad@users.noreply.github.com>
    Co-authored-by: Stefan Martín Lara <74354951+Stefanowich33@users.noreply.github.com>
    
    * changed to workflow_dispatch for testing
    
    Co-authored-by: Frederik Hounsvad <36563020+Hounsvad@users.noreply.github.com>
    Co-authored-by: Stefan Martín Lara <74354951+Stefanowich33@users.noreply.github.com>

---
+++ ci/development.sh
@@ -0,0 +1,2 @@
+docker run ghcr.io/stefanowich33/simple-fortune-cookie/cookie-backend:trunk --pull=always
+docker run ghcr.io/stefanowich33/simple-fortune-cookie/cookie-frontend:trunk --pull=always
---
+++ ci/production.sh
@@ -0,0 +1,2 @@
+docker run ghcr.io/stefanowich33/simple-fortune-cookie/cookie-backend:$0
+docker run ghcr.io/stefanowich33/simple-fortune-cookie/cookie-frontend:$0 --env BACKEND_DNS=backend
