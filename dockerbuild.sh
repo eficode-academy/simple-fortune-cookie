cc67e2c771cf9a3b981515e04b037c4a808c7e08
Point <christian@melsen.info>
2022-08-18 10:32:21 +0200

    Added push parameter to dockerbuild.sh

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build -t fortune_cookie_be -f Dockerfile_be .
 docker build -t fortune_cookie_fe -f Dockerfile_fe .
-docker image push fortune_cookie_be
-docker image push fortune_cookie_fe
+docker image push $1/fortune_cookie_be
+docker image push $1/fortune_cookie_fe
