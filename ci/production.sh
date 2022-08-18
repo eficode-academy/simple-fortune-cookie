7a4c49be76f65cfbcf4833414e6ed298d4b5f08c
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2022-08-18 20:01:23 +0200

    Update production.sh (#27)

--- ci/production.sh
+++ ci/production.sh
@@ -1,3 +1,3 @@
-docker kill production-backend production-frontend
+docker rm -f production-backend production-frontend
 docker run -d --name production-backend ghcr.io/stefanowich33/simple-fortune-cookie/cookie-backend:$0
 docker run -d --name production-frontend ghcr.io/stefanowich33/simple-fortune-cookie/cookie-frontend:$0 --env BACKEND_DNS=backend
