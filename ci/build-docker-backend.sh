e49d03a37befc5a06a347ee120540257e7ff3d15
MizzleTheHorse <emil.madsen@live.dk>
2022-08-18 11:37:41 +0000

    trying to fix workflow

--- ci/build-docker-backend.sh
+++ ci/build-docker-backend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-docker build -t "${DockerRepo}backend:latest" -t "${DockerRepo}backend-$Tag" /
+docker build -t "${DockerRepo}backend:latest" -t "${DockerRepo}backend-$Tag" backend/
