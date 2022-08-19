85197c23a1dbaec4b6f651c304e9c77c460ac02e
nicobah <nibah18@student.sdu.dk>
2022-08-19 18:49:15 +0000

    added kubetest to workflow

--- kubernetes/test.sh
+++ kubernetes/test.sh
@@ -1,8 +1,12 @@
-ready=$(curl $1:$2/api/all)
+port=$(kubectl get service | grep frontend | tr -s ' ' | cut -d ' ' -f 5 | cut -c4-8)
+echo port is $port
+ip=$(kubectl get nodes -o wide | head -n 2 | awk '{print $7}' | sed -n 2p)
+echo ip is $ip
+ready=$(curl $ip:$port/api/all)
 p=$(grep \<p\\\> <<< "$ready")
 if [ -z "$p" ]
 then
-  echo "0"
+  exit 1
 else
-  echo "1"
+  echo works #this should work
 fi
