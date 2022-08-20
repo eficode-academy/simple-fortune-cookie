232547affdca924605ba8aea50b8641659f97c8a
Marcus L. Jensen <lomztein4@gmail.com>
2022-08-20 11:03:05 +0000

    Added cluster run scripts

---
+++ kill.sh
@@ -0,0 +1,4 @@
+kubectl delete -f kubernetes/configs
+kubectl delete -f kubernetes/deployments
+kubectl delete -f kubernetes/services
+kubectl delete -f kubernetes/redis-pod.yaml
---
+++ run.sh
@@ -0,0 +1,4 @@
+kubectl apply -f kubernetes/configs
+kubectl apply -f kubernetes/deployments
+kubectl apply -f kubernetes/services
+kubectl apply -f kubernetes/redis-pod.yaml
