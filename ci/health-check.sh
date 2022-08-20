2a31ed6e2db72ff6a59c116fe8713b8096a4c264
devantler <nikolaiemildamm@icloud.com>
2022-08-20 19:55:22 +0000

    Updated healh-check

--- ci/health-check.sh
+++ ci/health-check.sh
@@ -4,8 +4,8 @@ sleep 180
 echo "Retrieving loadbalancer hostname from service 🚀"
 hostname=$( kubectl get services frontend --output jsonpath='{.status.loadBalancer.ingress[0].hostname}' )
 
-status_code=$( curl --head --location --connect-timeout 5 --write-out %{http_code} --silent --output /dev/null http://${hostname} )
-if [[ "$status_code" == 200 ]] ; then
+status=$( curl http://${hostname}/healthz )
+if [[ "$status" == "healthy" ]] ; then
     echo "The site is up! 🤘🏻🥳🤘🏻"
     echo "You can visit it on: $hostname"
 else
