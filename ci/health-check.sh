e6f29a283a9a4c34e26d7a356bdc28b9db3dea30
Stefan Martín Lara <74354951+Stefanowich33@users.noreply.github.com>
2022-08-20 10:42:22 +0200

    Update health-check.sh

--- ci/health-check.sh
+++ ci/health-check.sh
@@ -1,9 +1,9 @@
 echo "Testing wait command"
-sleep 5 &
+sleep 5
 
 instance_name=$1
-status_code=$(curl -Is http://${instance_name}.eficode.academy:8080/ | head -n 1)
-if [[ "$status_code" -ne 200 ]] ; then
+status_code=$(curl --head --location --connect-timeout 5 --write-out %{http_code} --silent --output /dev/null http://${instance_name}.eficode.academy:8080/)
+if [[ "$status_code" == 200 ]] ; then
     echo "The site is up!"
 else
     echo "The site is not running... :-("
