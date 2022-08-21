438752fae444c3d15531acbfd3c6bf4080edadfb
here <hendrik.reiter@ppi.de>
2022-08-21 11:45:30 +0200

    add k8s deployment

---
+++ ci/k8s.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+kubectl apply -f k8s/backend-deployment.yaml
+kubectl apply -f k8s/backend-service.yaml
+kubectl apply -f k8s/frontend-deployment.yaml
+kubectl apply -f k8s/frontend-service.yaml
