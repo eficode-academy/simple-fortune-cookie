383f41e2c2b4ff1cb86514742f387051136384b9
Fuad <fujam20@student.sdu>
2022-08-21 18:40:00 +0000

    added different enviroments dev and prod

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-docker build -t "${DockerRepo}simple-fortune-cookie-backend:latest" -t "${DockerRepo}simple-fortune-cookie-$1:1.0-$Tag" backend/
-docker build -t "${DockerRepo}simple-fortune-cookie-frontend:latest" -t "${DockerRepo}simple-fortune-cookie-$1:1.0-$Tag" frontend/
+docker build -t "${DockerRepo}simple-fortune-cookie-backend-$Tag:latest" -t "${DockerRepo}simple-fortune-cookie-$1:1.0-$Tag" backend/
+docker build -t "${DockerRepo}simple-fortune-cookie-frontend-$Tag:latest" -t "${DockerRepo}simple-fortune-cookie-$1:1.0-$Tag" frontend/
 
