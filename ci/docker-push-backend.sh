5bfc80e9f10461c4a1d6715bbad7c6e2c47fd15f
Sebastian <sejen20@sdu.student.dk>
2023-08-18 07:46:25 +0000

    Made shell files for push, for both frontend and backend

---
+++ ci/docker-push-backend.sh
@@ -0,0 +1,5 @@
+docker_username_lower=$(echo "$docker_username" | tr '[:upper:]' '[:lower:]')
+echo "$docker_password" | docker login ghcr.io --username "$docker_username_lower" --password-stdin
+docker push "ghcr.io/$docker_username_lower/simple-fortune-cookie-backend:1.0-${GIT_COMMIT::8}"
+docker push "ghcr.io/$docker_username_lower/simple-fortune-cookie-backend:latest" &
+wait
