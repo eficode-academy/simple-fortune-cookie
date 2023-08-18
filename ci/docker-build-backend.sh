9e4812a88328cd687bc7514036450706190410c2
Sebastian <sejen20@sdu.student.dk>
2023-08-18 07:47:38 +0000

    Made shell files for buikd, for both frontend and backend

---
+++ ci/docker-build-backend.sh
@@ -0,0 +1,4 @@
+docker_username_lower=$(echo "$docker_username" | tr '[:upper:]' '[:lower:]')
+[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
+[[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="ghcr.io/${docker_username_lower}/"
+docker build -t "${DockerRepo}simple-fortune-cookie-backend:latest" -t "${DockerRepo}simple-fortune-cookie-backend:1.0-$Tag" backend/
