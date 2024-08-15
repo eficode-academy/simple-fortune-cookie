15dd0eb043a2c7394e10ffaefa5a7131edeffa10
Yalcin Acili <112946492+Yalcinva03@users.noreply.github.com>
2024-08-15 13:55:08 +0200

    fixed names build-docker.sh

--- frontend/ci/build-docker.sh
+++ frontend/ci/build-docker.sh
@@ -2,4 +2,4 @@
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}" 
 REPO="ghcr.io/$docker_username/"
 echo "${REPO}"
-docker build -t "${REPO}backend:latest" -t "${REPO}backend:1.0-$Tag" frontend/
+docker build -t "${REPO}frontend:latest" -t "${REPO}frontend:1.0-$Tag" frontend/
