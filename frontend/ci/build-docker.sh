2c1c4b2cca9e26abd26f0792845e8105a6bf612a
Yalcin Acili <112946492+Yalcinva03@users.noreply.github.com>
2024-08-15 13:53:30 +0200

    Create build-docker.sh for frontend

---
+++ frontend/ci/build-docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}" 
+REPO="ghcr.io/$docker_username/"
+echo "${REPO}"
+docker build -t "${REPO}backend:latest" -t "${REPO}backend:1.0-$Tag" frontend/
