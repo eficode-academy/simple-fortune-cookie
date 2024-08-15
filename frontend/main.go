b43c223f8ae5d22d59954dbc63dad1744298d44b
ks <svane.kasper20@gmail.com>
2024-08-15 09:08:25 +0200

    Update main.go so the test can run

--- frontend/main.go
+++ frontend/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    "io"
-    "fmt"
-    "encoding/json"
-    "html/template"
-    "net/http"
-    "log"
-    "time"
-    "bytes"
-    "math/rand"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
 )
 
-var BACKEND_DNS=getEnv("BACKEND_DNS", "localhost")
-var BACKEND_PORT=getEnv("BACKEND_PORT", "9000")
+var BACKEND_DNS = getEnv("BACKEND_DNS", "localhost")
+var BACKEND_PORT = getEnv("BACKEND_PORT", "9000")
 
 type fortune struct {
 	ID      string `json:"id" redis:"id"`
@@ -21,85 +21,85 @@ type fortune struct {
 }
 
 type newFortune struct {
-    Message string `json:"message"`
+	Message string `json:"message"`
 }
 
 // use a custom client, because we don't do blocking operations wihout timeouts
 var myClient = &http.Client{Timeout: 10 * time.Second}
 
 func HealthzHandler(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusOK)
-    io.WriteString(w, "healthy")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "healthy")
 }
 
 func main() {
 
-    http.HandleFunc("/healthz", HealthzHandler)
+	http.HandleFunc("/healthz", HealthzHandler)
 
-    http.HandleFunc("/api/random", func (w http.ResponseWriter, r *http.Request) {
-        resp, err := myClient.Get(fmt.Sprintf("http://%s:%s/fortunes/random", BACKEND_DNS, BACKEND_PORT))
-        if err != nil {
-            log.Fatalln(err)
-            fmt.Fprint(w, err)
-            return
-        }
+	http.HandleFunc("/api/random", func(w http.ResponseWriter, r *http.Request) {
+		resp, err := myClient.Get(fmt.Sprintf("http://%s:%s/fortunes/random", BACKEND_DNS, BACKEND_PORT))
+		if err != nil {
+			log.Fatalln(err)
+			fmt.Fprint(w, err)
+			return
+		}
 
-        f := new(fortune)
-        json.NewDecoder(resp.Body).Decode(f)
+		f := new(fortune)
+		json.NewDecoder(resp.Body).Decode(f)
 
-        fmt.Fprint(w, f.Message)
-        return
-    })
+		fmt.Fprint(w, f.Message)
+		return
+	})
 
-    http.HandleFunc("/api/all", func (w http.ResponseWriter, r *http.Request) {
-        resp, err := myClient.Get(fmt.Sprintf("http://%s:%s/fortunes", BACKEND_DNS, BACKEND_PORT))
-        if err != nil {
-            log.Fatalln(err)
-            fmt.Fprint(w, err)
-            return
-        }
+	http.HandleFunc("/api/all", func(w http.ResponseWriter, r *http.Request) {
+		resp, err := myClient.Get(fmt.Sprintf("http://%s:%s/fortunes", BACKEND_DNS, BACKEND_PORT))
+		if err != nil {
+			log.Fatalln(err)
+			fmt.Fprint(w, err)
+			return
+		}
 
-        fortunes := new([]fortune)
-        json.NewDecoder(resp.Body).Decode(fortunes)
+		fortunes := new([]fortune)
+		json.NewDecoder(resp.Body).Decode(fortunes)
 
-        tmpl, err := template.ParseFiles("./templates/fortunes.html")
+		tmpl, err := template.ParseFiles("./templates/fortunes.html")
 
-        if err != nil {
-            log.Fatalln(err)
-            fmt.Fprint(w, err)
-            return
-        }
+		if err != nil {
+			log.Fatalln(err)
+			fmt.Fprint(w, err)
+			return
+		}
 
-        tmpl.Execute(w, fortunes)
-        return
-    })
+		tmpl.Execute(w, fortunes)
+		return
+	})
 
-    http.HandleFunc("/api/add", func (w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/api/add", func(w http.ResponseWriter, r *http.Request) {
 
-        if r.Method != "POST" {
-            http.Error(w, "Use POST", http.StatusMethodNotAllowed)
-            return
-        }
+		if r.Method != "POST" {
+			http.Error(w, "Use POST", http.StatusMethodNotAllowed)
+			return
+		}
 
-        f := new(newFortune)
-        json.NewDecoder(r.Body).Decode(f)
+		f := new(newFortune)
+		json.NewDecoder(r.Body).Decode(f)
 
-        var postUrl = fmt.Sprintf("http://%s:%s/fortunes", BACKEND_DNS, BACKEND_PORT)
-        var jsonStr = []byte(fmt.Sprintf(`{"id": "%d", "message": "%s"}`, rand.Intn(10000), f.Message))
+		var postUrl = fmt.Sprintf("http://%s:%s/fortunes", BACKEND_DNS, BACKEND_PORT)
+		var jsonStr = []byte(fmt.Sprintf(`{"id": "%d", "message": "%s"}`, rand.Intn(10000), f.Message))
 
-        _, err := myClient.Post(postUrl, "application/json", bytes.NewBuffer(jsonStr))
-        if err != nil {
-            log.Fatalln(err)
-            fmt.Fprint(w, err)
-            return
-        }
+		_, err := myClient.Post(postUrl, "application/json", bytes.NewBuffer(jsonStr))
+		if err != nil {
+			log.Fatalln(err)
+			fmt.Fprint(w, err)
+			return
+		}
 
-        fmt.Fprint(w, "Cookie added!")
+		fmt.Fprint(w, "Cookie added!")
 
-        return
-    })
+		return
+	})
 
-    http.Handle("/", http.FileServer(http.Dir("./static")))
-    err := http.ListenAndServe(":8080", nil)
-    fmt.Println("%v", err)
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	err := http.ListenAndServe(":8080", nil)
+	fmt.Printf("%v\n", err)
 }
