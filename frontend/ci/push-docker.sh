fe7f01c2279d759afad588bbd663f19f19a1a585
Yalcin Acili <112946492+Yalcinva03@users.noreply.github.com>
2024-08-15 13:54:34 +0200

    Create push-docker.sh for frontend

---
+++ frontend/ci/push-docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "$docker_password" | docker login ghcr.io --username "$docker_username" --password-stdin
+docker push "ghcr.io/$docker_username/frontend:1.0-${GIT_COMMIT::8}" 
+docker push "ghcr.io/$docker_username/frontend:latest" &
+wait
