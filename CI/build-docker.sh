ebfe7ae4b88eae0c154af054134c737cd65762c1
Gamze Basoda <m.g.basoda@hotmail.com>
2024-08-17 18:21:03 +0000

    docker build changed

--- CI/build-docker.sh
+++ CI/build-docker.sh
@@ -2,4 +2,4 @@
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}" 
 REPO="ghcr.io/$docker_username/"
 echo "${REPO}"
-docker build -t "${REPO}micronaut-app:latest" -t "${REPO}micronaut-app:1.0-$Tag" app/
+docker build -t "${REPO}micronaut-app:latest" -t "${REPO}micronaut-app:1.0-$Tag" simple-fortune-cookie/
