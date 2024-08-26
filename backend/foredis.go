8f7758e7d6a1fe68e44154d67f64eaa3e1276e03
eficode-academy <github-bot@eficode.com>
2024-08-26 18:17:54 +0000

    https://github.com/eficode-academy/simple-fortune-cookie-dev/commit/42623109d507b21189dd8f36fac6c38f35e3714a from refs/heads/main

--- backend/foredis.go
+++ backend/foredis.go
@@ -5,36 +5,52 @@ import (
 	"github.com/gomodule/redigo/redis"
 	"log"
 	"sync"
+	"time"
+	"os"
 )
 
 var dbLink redis.Conn
 var usingRedis = false
 
 func init() {
-	conn, err := redis.Dial("tcp", fmt.Sprintf("%s:6379", getEnv("REDIS_DNS", "localhost")))
-	if err != nil {
-		log.Println("redis", err)
-	} else {
-		dbLink = conn
-		usingRedis = true
+	// Check if REDIS_DNS environment variable is set
+	if os.Getenv("REDIS_DNS") == "" {
+		fmt.Println("redis config not set")
+		return
+	}
+	var err error
+	for i := 0; i < 5; i++ {
+		dbLink, err = redis.Dial("tcp", fmt.Sprintf("%s:6379", getEnv("REDIS_DNS", "localhost")))
+		if err == nil {
+			usingRedis = true
+			break
+		}
+		log.Printf("Attempt %d: redis connection failed: %s", i+1, err)
+		time.Sleep(2 * time.Second)
+	}
 
-		resKeys, err := redis.Values(dbLink.Do("hkeys", "fortunes"))
+	if !usingRedis {
+		log.Println("Failed to connect to redis after 5 attempts")
+		return
+	}
+
+	resKeys, err := redis.Values(dbLink.Do("hkeys", "fortunes"))
+	if err != nil {
+		fmt.Println("redis hkeys failed", err.Error())
+		return
+	}
+
+	datastoreDefault = datastore{m: map[string]fortune{}, RWMutex: &sync.RWMutex{}}
+	fmt.Printf("*** loading redis fortunes:\n")
+	for _, key := range resKeys {
+		val, err := dbLink.Do("hget", "fortunes", key)
 		if err != nil {
-			fmt.Println("redis hkeys failed", err.Error())
+			fmt.Println("redis hget failed", err.Error())
 		} else {
-			datastoreDefault = datastore{m: map[string]fortune{}, RWMutex: &sync.RWMutex{}}
-			fmt.Printf("*** loading redis fortunes:\n")
-			for _, key := range resKeys {
-				val, err := dbLink.Do("hget", "fortunes", key)
-				if err != nil {
-					fmt.Println("redis hget failed", err.Error())
-				} else {
-					idx := fmt.Sprintf("%s", key.([]byte))
-					msg := fmt.Sprintf("%s", val.([]byte))
-					datastoreDefault.m[idx] = fortune{ID: idx, Message: msg}
-					fmt.Printf("%s => %s\n", key, val)
-				}
-			}
+			idx := fmt.Sprintf("%s", key.([]byte))
+			msg := fmt.Sprintf("%s", val.([]byte))
+			datastoreDefault.m[idx] = fortune{ID: idx, Message: msg}
+			fmt.Printf("%s => %s\n", key, val)
 		}
 	}
 }
